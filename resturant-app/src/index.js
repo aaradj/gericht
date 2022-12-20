9d578db3af4c9826d308636b96513cfc7dc9ca1c
Arad Jahangiri <aradjahangiri84@gmail.com>
2022-12-20 20:49:12 +0330

    first commit

---
+++ resturant-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM, {createRoot} from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+const root=createRoot(document.getElementById("root"));
+root.render(<App/>);
